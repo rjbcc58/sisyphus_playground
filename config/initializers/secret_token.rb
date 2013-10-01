80f187eb7b2120ec68e39475f07834cb8ac010fb
Gowiem <MGowie@apprenaissance.com>
2013-10-01 16:34:49 -0400

    Adds initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SisyphusPlayground::Application.config.secret_key_base = '18596e0ac756eeac3fb5d2208fc2d8c990debd9e6a3b8319b79a6f2aeb60cb56346b60b0f4bc7580023e0fa006b93ab1529b62169a49b74409afd43248717100'
